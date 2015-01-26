5131af4a8f2f9b0ecd1f437d4f2c767cb819970c
ja2466633 <joanaarellano0@gmail.com>
2015-01-26 11:50:02 -0800

    Signed-off-by: ja2466633 <joanaarellano0@gmail.com>

---
+++ Project1/Suduko/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on January 26, 2015, 10:50 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
