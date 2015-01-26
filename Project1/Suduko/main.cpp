36186fd3329f89fb89c4221a5e4a879500694e47
ja2466633 <joanaarellano0@gmail.com>
2015-01-26 11:56:50 -0800

    Signed-off-by: ja2466633 <joanaarellano0@gmail.com>

--- Project1/Suduko/main.cpp
+++ Project1/Suduko/main.cpp
@@ -1,19 +1,20 @@
 /* 
  * File:   main.cpp
- * Author: rcc
- *
+ * Author: Joana Arellano
  * Created on January 26, 2015, 10:50 AM
+ * Purpose: 
  */
 
+//System Libraries
 #include <cstdlib>
-
+#include <iostream>
 using namespace std;
-
-/*
- * 
- */
-int main(int argc, char** argv) {
-
+//User Libraries
+//Global Constant
+//Function Prototype
+//Execution Begins
+int main(int argc, char** argv) 
+{
     return 0;
 }
 
